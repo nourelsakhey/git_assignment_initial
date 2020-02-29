9000119a2c63e1bcbbcdd20b491e1299c7c3028b
nourelsakhey <36204564+nourelsakhey@users.noreply.github.com>
2020-02-29 12:22:48 +0200

    20170320
    20170320

---
+++ src/CircleCircumference.java
@@ -0,0 +1,17 @@
+
+public class CircleCircumference implements ISubscriber {
+
+    public CircleCircumference() {
+
+    }
+ 
+    @Override
+	public void notifySubscriber(String input){
+           double x= Double.parseDouble(input);
+           double y=2*3.14*x;
+            System.out.print("the circumference equals ");
+            System.out.println(y);
+        }
+       
+    
+}
