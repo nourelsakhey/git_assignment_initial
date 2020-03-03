0d51a60d3cc1d8ede3bbd4f2bfe633dfd509d25b
eman ayman <emanayman657@yahoo.com>
2020-03-03 20:28:34 +0200

    new

--- src/BallVolume.java
+++ src/BallVolume.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author Eman Ayman Khalaf
+ * @author Eman Ayman Khalaf 20170067
  */
 import java.util.Scanner;
 
@@ -14,7 +14,7 @@ public class BallVolume implements ISubscriber {
     public void notifySubscriber(String input) {
         int radius= Integer.parseInt(input);
         double pie=3.14285714286;
-        double volume=(4.0/3.0)*pie*(radius*radius*radius);
+        double volume=(4.0/3.0)*pie*(radius*radius*radius); // the volume of ball
         System.out.println("Volume of the sphere="+volume);
     }
 
